0e957ee50447aec6e75d80b0c62aef24162a27bf
Alexander Krylovsky PC <cryls@yandex.ru>
2024-11-02 20:47:46 +0300

    initial - v.0.02

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+// import { useState } from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <>
+      <h1>User To-Do Table</h1>
+      <p>User task table for effective planning</p>
+      <div>TABLE</div>
+    </>
+  );
+}
+
+export default App;
